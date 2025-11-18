6acfdf0e1711075cec9451bb9d446a3f7dc15989
Proregress <huishouzhigumd@163.com>
2025-11-18 22:59:01 +0800

    0x5 交易指令与参数解析

--- rc_dex/consumer/internal/logic/block/pump.go
+++ rc_dex/consumer/internal/logic/block/pump.go
@@ -2,6 +2,7 @@ package block
 
 import (
 	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 
@@ -29,12 +30,146 @@ func DecodePumpFunAMMInstruction(inst *types.CompiledInstruction, tx *client.Blo
 	discriminator := GetInstructionDiscriminator(inst.Data)
 
 	if bytes.Equal(discriminator, PumpAmmBuyDiscriminator) {
-		fmt.Println("AMM Buy instruction")
+		DecodePumpFunAMMBuyInstruction(inst, tx)
 	} else if bytes.Equal(discriminator, PumpAmmSellDiscriminator) {
-		fmt.Println("AMM Sell instruction")
+		DecodePumpFunAMMSellInstruction(inst, tx)
 	} else {
 		return errors.New("unknown instruction discriminator")
 	}
+
+	// 解析账户数据: 回顾交易解析（账户存储结构、指令数据结构）
+	if len(inst.Accounts) != 21 && len(inst.Accounts) != 23 {
+		return fmt.Errorf("invalid accounts length: %d", len(inst.Accounts))
+	}
+
+	return
+}
+
+func DecodePumpFunAMMBuyInstruction(inst *types.CompiledInstruction, tx *client.BlockTransaction) (err error) {
+	fmt.Println("pump.fun AMM Buy instruction", base58.Encode(tx.Transaction.Signatures[0]))
+	// 解析账户数据: 回顾交易解析（账户存储结构、指令数据结构）
+	if len(inst.Accounts) != 23 {
+		return fmt.Errorf("invalid accounts length: %d", len(inst.Accounts))
+	}
+
+	// 参考源1:IDL文件
+	// 参考源2:IDL文件生成的instructions.go文件
+	pool := tx.AccountKeys[inst.Accounts[0]]
+	user := tx.AccountKeys[inst.Accounts[1]]
+	globalConfig := tx.AccountKeys[inst.Accounts[2]]
+	baseMint := tx.AccountKeys[inst.Accounts[3]]
+	quoteMint := tx.AccountKeys[inst.Accounts[4]]
+	userBaseTokenAccount := tx.AccountKeys[inst.Accounts[5]]
+	userQuoteTokenAccount := tx.AccountKeys[inst.Accounts[6]]
+	poolBaseTokenAccount := tx.AccountKeys[inst.Accounts[7]]
+	poolQuoteTokenAccount := tx.AccountKeys[inst.Accounts[8]]
+	protocolFeeRecipient := tx.AccountKeys[inst.Accounts[9]]
+	protocolFeeRecipientTokenAccount := tx.AccountKeys[inst.Accounts[10]]
+	baseTokenProgram := tx.AccountKeys[inst.Accounts[11]]
+	quoteTokenProgram := tx.AccountKeys[inst.Accounts[12]]
+	systemProgram := tx.AccountKeys[inst.Accounts[13]]
+	associatedTokenProgram := tx.AccountKeys[inst.Accounts[14]]
+	eventAuthority := tx.AccountKeys[inst.Accounts[15]]
+	program := tx.AccountKeys[inst.Accounts[16]]
+	coinCreatorVaultAta := tx.AccountKeys[inst.Accounts[17]]
+	coinCreatorVaultAuthority := tx.AccountKeys[inst.Accounts[18]]
+	globalVolumeAccumulator := tx.AccountKeys[inst.Accounts[19]]
+	userVolumeAccumulator := tx.AccountKeys[inst.Accounts[20]]
+	feeConfig := tx.AccountKeys[inst.Accounts[21]]
+	feeProgram := tx.AccountKeys[inst.Accounts[22]]
+
+	fmt.Println("------------------- buy accounts ------------------")
+
+	fmt.Println("pool:", pool.String())
+	fmt.Println("user:", user.String())
+	fmt.Println("globalConfig:", globalConfig.String())
+	fmt.Println("baseMint:", baseMint.String())
+	fmt.Println("quoteMint:", quoteMint.String())
+	fmt.Println("userBaseTokenAccount:", userBaseTokenAccount.String())
+	fmt.Println("userQuoteTokenAccount:", userQuoteTokenAccount.String())
+	fmt.Println("poolBaseTokenAccount:", poolBaseTokenAccount.String())
+	fmt.Println("poolQuoteTokenAccount:", poolQuoteTokenAccount.String())
+	fmt.Println("protocolFeeRecipient:", protocolFeeRecipient.String())
+	fmt.Println("protocolFeeRecipientTokenAccount:", protocolFeeRecipientTokenAccount.String())
+	fmt.Println("baseTokenProgram:", baseTokenProgram.String())
+	fmt.Println("quoteTokenProgram:", quoteTokenProgram.String())
+	fmt.Println("systemProgram:", systemProgram.String())
+	fmt.Println("associatedTokenProgram:", associatedTokenProgram.String())
+	fmt.Println("eventAuthority:", eventAuthority.String())
+	fmt.Println("program:", program.String())
+	fmt.Println("coinCreatorVaultAta:", coinCreatorVaultAta.String())
+	fmt.Println("coinCreatorVaultAuthority:", coinCreatorVaultAuthority.String())
+	fmt.Println("globalVolumeAccumulator:", globalVolumeAccumulator.String())
+	fmt.Println("userVolumeAccumulator:", userVolumeAccumulator.String())
+	fmt.Println("feeConfig:", feeConfig.String())
+	fmt.Println("feeProgram:", feeProgram.String())
+
+	fmt.Println("--------------------- buy data --------------------")
+
+	fmt.Println("baseAmountOut:", binary.LittleEndian.Uint64(inst.Data[8:16]))
+	fmt.Println("maxQuoteAmountIn:", binary.LittleEndian.Uint64(inst.Data[16:24]))
+
+	return
+}
+
+func DecodePumpFunAMMSellInstruction(inst *types.CompiledInstruction, tx *client.BlockTransaction) (err error) {
+	fmt.Println("pump.fun AMM Sell instruction", base58.Encode(tx.Transaction.Signatures[0]))
+	// 解析账户数据: 回顾交易解析（账户存储结构、指令数据结构）
+	if len(inst.Accounts) != 21 {
+		return fmt.Errorf("invalid accounts length: %d", len(inst.Accounts))
+	}
+
+	pool := tx.AccountKeys[inst.Accounts[0]]
+	user := tx.AccountKeys[inst.Accounts[1]]
+	globalConfig := tx.AccountKeys[inst.Accounts[2]]
+	baseMint := tx.AccountKeys[inst.Accounts[3]]
+	quoteMint := tx.AccountKeys[inst.Accounts[4]]
+	userBaseTokenAccount := tx.AccountKeys[inst.Accounts[5]]
+	userQuoteTokenAccount := tx.AccountKeys[inst.Accounts[6]]
+	poolBaseTokenAccount := tx.AccountKeys[inst.Accounts[7]]
+	poolQuoteTokenAccount := tx.AccountKeys[inst.Accounts[8]]
+	protocolFeeRecipient := tx.AccountKeys[inst.Accounts[9]]
+	protocolFeeRecipientTokenAccount := tx.AccountKeys[inst.Accounts[10]]
+	baseTokenProgram := tx.AccountKeys[inst.Accounts[11]]
+	quoteTokenProgram := tx.AccountKeys[inst.Accounts[12]]
+	systemProgram := tx.AccountKeys[inst.Accounts[13]]
+	associatedTokenProgram := tx.AccountKeys[inst.Accounts[14]]
+	eventAuthority := tx.AccountKeys[inst.Accounts[15]]
+	program := tx.AccountKeys[inst.Accounts[16]]
+	coinCreatorVaultAta := tx.AccountKeys[inst.Accounts[17]]
+	coinCreatorVaultAuthority := tx.AccountKeys[inst.Accounts[18]]
+	feeConfig := tx.AccountKeys[inst.Accounts[19]]
+	feeProgram := tx.AccountKeys[inst.Accounts[20]]
+
+	fmt.Println("------------------- sell accounts ------------------")
+
+	fmt.Println("pool:", pool.String())
+	fmt.Println("user:", user.String())
+	fmt.Println("globalConfig:", globalConfig.String())
+	fmt.Println("baseMint:", baseMint.String())
+	fmt.Println("quoteMint:", quoteMint.String())
+	fmt.Println("userBaseTokenAccount:", userBaseTokenAccount.String())
+	fmt.Println("userQuoteTokenAccount:", userQuoteTokenAccount.String())
+	fmt.Println("poolBaseTokenAccount:", poolBaseTokenAccount.String())
+	fmt.Println("poolQuoteTokenAccount:", poolQuoteTokenAccount.String())
+	fmt.Println("protocolFeeRecipient:", protocolFeeRecipient.String())
+	fmt.Println("protocolFeeRecipientTokenAccount:", protocolFeeRecipientTokenAccount.String())
+	fmt.Println("baseTokenProgram:", baseTokenProgram.String())
+	fmt.Println("quoteTokenProgram:", quoteTokenProgram.String())
+	fmt.Println("systemProgram:", systemProgram.String())
+	fmt.Println("associatedTokenProgram:", associatedTokenProgram.String())
+	fmt.Println("eventAuthority:", eventAuthority.String())
+	fmt.Println("program:", program.String())
+	fmt.Println("coinCreatorVaultAta:", coinCreatorVaultAta.String())
+	fmt.Println("coinCreatorVaultAuthority:", coinCreatorVaultAuthority.String())
+	fmt.Println("feeConfig:", feeConfig.String())
+	fmt.Println("feeProgram:", feeProgram.String())
+
+	fmt.Println("--------------------- sell data --------------------")
+
+	fmt.Println("baseAmountIn:", binary.LittleEndian.Uint64(inst.Data[8:16]))
+	fmt.Println("minQuoteAmountOut:", binary.LittleEndian.Uint64(inst.Data[16:24]))
+
 	return
 }
 
